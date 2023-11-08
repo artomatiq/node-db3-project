98802d2dcdf654e8605c0533a53e0959f5b77cc1
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-08 14:26:03 -0500

    global error handling set up

--- api/server.js
+++ api/server.js
@@ -7,4 +7,17 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use('*', (req, res, next) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        message: error.message || 'error',
+        stack: error.stack
+    })
+})
+
 module.exports = server;
